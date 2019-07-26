9b291704bb98343a361b6b35119675f71df93386
Ayoub allali <ayoub.allali@schibsted.com>
2019-07-26 14:34:09 +0100

    fix(lint): fix the some TS linting

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,30 @@
 import express from "express";
 
-import { MinisterCrawler, Status, LicensePlate} from './opposition/crawler';
+import { ILicensePlate, MinisterCrawler, Status} from "./opposition/crawler";
 
 const app = express();
 const port = 8080 || process.env.PORT;
 
 const crawler = new MinisterCrawler();
 
-
-const parseLicenseFrom = (license: string): LicensePlate => {
-  const parts = license.replace('"','').split('-');
-  if(parts.length === 3 && !parts.some((elm => (elm == null || elm === ''))) ) {
+const parseLicenseFrom = (license: string): ILicensePlate => {
+  const parts = license.replace('"', "").split("-");
+  if (parts.length === 3 && !parts.some(((elm) => (elm == null || elm === ""))) ) {
     return {
-      VehiculeID: parts[0],
       RegionKey1: parts[1],
       RegionKey2: parts[2],
-    }
+      VehiculeID: parts[0],
+    };
   }
 
   return null;
-}
+};
 
 app.get("/api/v1/car/status", async (req, res) => {
-  const licenseParam = req.query['license'];
-  console.log(licenseParam);
+  const licenseParam = req.query.license;
   const license = parseLicenseFrom(licenseParam);
 
-  if(!license){
+  if (!license) {
     res.status(400);
     return res.json({
       error: "license plate format is incorrect",
@@ -34,10 +32,10 @@ app.get("/api/v1/car/status", async (req, res) => {
   }
 
   const result = await crawler.getVehiculeStatus(license);
-  
+
   res.json({
     statusCode: result,
-    statusName: (result === Status.Valide)?'No opposition': 'Vehicule not found or with opposition',
+    statusName: (result === Status.Valide) ? "No opposition" : "Vehicule not found or with opposition",
     ts: + new Date(),
   });
 });
--- src/opposition/crawler.ts
+++ src/opposition/crawler.ts
@@ -1,84 +1,80 @@
-const request = require('request-promise');
-const cheerio = require('cheerio');
+import cheerio from "cheerio";
+import request from "request-promise";
 
 export enum Status {
-  Valide = '1',
-  Invalide_not_found = '0',
-  Err = '-1',
+  Valide = "1",
+  Invalide_not_found = "0",
+  Err = "-1",
 }
 
-export interface LicensePlate {
-  VehiculeID: string,
-  RegionKey1: string,
-  RegionKey2: string,
+export interface ILicensePlate {
+  VehiculeID: string;
+  RegionKey1: string;
+  RegionKey2: string;
 }
 
-export interface Crawler {
-  getVehiculeStatus(licensePlate: LicensePlate): Promise<Status>
+export interface ICrawler {
+  getVehiculeStatus(licensePlate: ILicensePlate): Promise<Status>;
 }
 
-export class MinisterCrawler implements Crawler {
-  private URI = 'http://www.assiaqacard.ma/opppub/';
-  private OK_IMAGE_NAME = 'images/Visuel_Etat-opposition_Non.png';
-  private KO_IMAGE_NAME = 'images/Visuel_Etat-opposition_Oui.png';
-  private RESP_SELECTOR = '.ts-1-26 > img';
+export class MinisterCrawler implements ICrawler {
+  private URI = "http://www.assiaqacard.ma/opppub/";
+  private OK_IMAGE_NAME = "images/Visuel_Etat-opposition_Non.png";
+  private KO_IMAGE_NAME = "images/Visuel_Etat-opposition_Oui.png";
+  private RESP_SELECTOR = ".ts-1-26 > img";
 
   private REGION_KEY_MAP = {
-      'A':'أ',
-      'B':'ب',
-      'D':'د',
-      'H':'ھ',
-      'O':'و',
-    }
+      A: "أ",
+      B: "ب",
+      D: "د",
+      H: "ھ",
+      O: "و",
+    };
 
-
-
-  public async getVehiculeStatus(licensePlate: LicensePlate): Promise<Status> {
+  public async getVehiculeStatus(licensePlate: ILicensePlate): Promise<Status> {
     const resp  = await this.send(licensePlate);
 
     return this.parseResponse(resp);
   }
 
   private parseResponse(resp: string): Status {
-    if(resp === undefined) {
+    if (resp === undefined) {
       return null;
     }
 
-    if(resp == this.OK_IMAGE_NAME) {
+    if (resp === this.OK_IMAGE_NAME) {
       return Status.Valide;
     }
 
-    if(resp == this.KO_IMAGE_NAME) {
+    if (resp === this.KO_IMAGE_NAME) {
       return Status.Invalide_not_found;
     }
 
     return Status.Invalide_not_found;
   }
 
-  private send(plate: LicensePlate): Promise<string> {
+  private send(plate: ILicensePlate): Promise<string> {
     const options = {
-      url:this.URI ,
-      transform: (body: string ) => {
-        const $ = cheerio.load(body);
-
-        return $(this.RESP_SELECTOR).attr('src');
-      },
       form: {
         immat1: plate.VehiculeID,
         immat2: this.mapRegionKey(plate.RegionKey1),
         immat3: plate.RegionKey2,
-        type: 'V',
-         suivant:  null,
-      }
-    }
+        suivant:  null,
+        type: "V",
+      },
+      transform: (body: string ) => {
+        const $ = cheerio.load(body);
 
-    console.log(options);
+        return $(this.RESP_SELECTOR).attr("src");
+      },
+        url: this.URI ,
+      };
 
     return request.post(options);
   }
 
   private mapRegionKey(key: string): string {
-    if(this.REGION_KEY_MAP[key]){
+    if (this.REGION_KEY_MAP[key]) {
       return this.REGION_KEY_MAP[key];
     }
 
@@ -86,4 +82,3 @@ export class MinisterCrawler implements Crawler {
   }
 
 }
-
